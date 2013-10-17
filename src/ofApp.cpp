a3e7e71685d52fe7bcbbefe8df3184bc3a168e20
Jesse Scott <jesse@memelab.ca>
2013-10-17 15:56:20 -0700

    Updated cpp with Leos fix

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,7 +5,6 @@ void ofApp::setup(){
     
     // Screen
     ofBackground(255);
-    ofSetVerticalSync(true);
     
     // Font
     font.loadFont("font/Courier New Bold.ttf", 9);
@@ -27,56 +26,64 @@ void ofApp::setup(){
     tmpB = 0;
     tmpC = 0;
     
-    vector<ofVideoDevice> devices = camera.listDevices();
-	if(verbose) {
-        for(int i = 0; i < devices.size(); i++){
-            cout << devices[i].id << ": " << devices[i].deviceName;
-            if( devices[i].bAvailable ){
-                cout << endl;
-            }else{
-                cout << " - unavailable " << endl;
-            }
-        }
-    }
-	camera.setDeviceID(4);
+    /*  vector<ofVideoDevice> devices = camera.listDevices();
+     if(verbose) {
+     for(int i = 0; i < devices.size(); i++){
+     cout << devices[i].id << ": " << devices[i].deviceName;
+     if( devices[i].bAvailable ){
+     cout << endl;
+     }else{
+     cout << " - unavailable " << endl;
+     }
+     }
+     }*/
+	camera.setDeviceID(0);
 	camera.setDesiredFrameRate(60);
     camera.setVerbose(true);
     camera.initGrabber(camWidth, camHeight);
     
-    videoInverted = new unsigned char[camWidth*camHeight*3];
-	videoTexture.allocate(camWidth, camHeight, GL_RGB);
-
+    videoInverted 	= new unsigned char[camWidth*camHeight*3];
+	videoTexture.allocate(camWidth,camHeight, GL_RGB);
+    
     cout << " -- END OF SETUP -- " << endl;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
     // Camera
     camera.update();
     if (camera.isFrameNew()){
         
         pixels = camera.getPixels();
-        int totalPixels = camWidth * camHeight * 3;
+        int totalPixels = camWidth * camHeight;
         lineCounter = 0;
-        unsigned char tmpR = 0;
-        unsigned char tmpG = 0;
-        unsigned char tmpB = 0;
-        unsigned char tmpC = 0;
+        int tmpR = 0;
+        int tmpG = 0;
+        int tmpB = 0;
+        
+        int tempCounter = 0;
         
         for (int i = 0; i < totalPixels; i++) {
-            videoInverted[i] = 255 - pixels[i];
+            
+            //  videoInverted[i] = 255 - pixels[i];
             
             // Adding Colors
-            tmpR += pixels[i];
-            tmpG += pixels[i+1];
-            tmpB += pixels[i+2];
+            tmpR += pixels[i*3];
+            tmpG += pixels[i*3+1];
+            tmpB += pixels[i*3+2];
             //tmpC += pixels[i];
-            
+            tempCounter++;
             // Store Color
             if(i % camWidth == 0) {
-                //cout << "TEMP CEE FOR LINE #" << lineCounter << " IS " << color << endl;
+                // get the average value
                 
+                ofLogNotice(ofToString(totalPixels));
+                ofLogNotice(ofToString(tmpR));
+                tmpR = tmpR/camWidth;
+                tmpG = tmpG/camWidth;
+                tmpB = tmpB/camWidth;
+                ofLogNotice("tempR = " + ofToString(tmpR));
                 // Set Avg Colours To Color Array
                 lineColors[lineCounter].r = int(tmpR);
                 lineColors[lineCounter].g = int(tmpG);
@@ -86,14 +93,13 @@ void ofApp::update(){
                 tmpR = 0;
                 tmpG = 0;
                 tmpB = 0;
-                //tmpC = 0;
                 
                 // Iterate
                 lineCounter++;
                 
             }
         }
-        //videoTexture.loadData(videoInverted, camWidth,camHeight, GL_RGB);
+        // videoTexture.loadData(videoInverted, camWidth,camHeight, GL_RGB);
 	}
     
 }
@@ -104,7 +110,7 @@ void ofApp::draw(){
     // Raw Camera
     ofSetColor(255);
     camera.draw(50, 50, camWidth, camHeight);
-    //videoTexture.draw(50 ,camHeight + 50, camWidth, camHeight);
+    // videoTexture.draw(50 ,camHeight + 50, camWidth, camHeight);
     
     // Lines
     for (int i = 0; i < camHeight; i++) {
@@ -121,7 +127,7 @@ void ofApp::draw(){
     char fpsStr[255];
     sprintf(fpsStr, "frame rate: %f", ofGetFrameRate());
     ofDrawBitmapString(fpsStr, 50, 700);
-
+    
 }
 
 //--------------------------------------------------------------
@@ -137,7 +143,7 @@ void ofApp::exit() {
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
     
     // Camera Settings
     if (key == 's' || key == 'S'){
@@ -147,40 +153,40 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }

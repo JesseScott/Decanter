5398333f97dcfb1b5fef6e7099b6f49c552a07be
Jesse Scott <jesse@memelab.ca>
2013-10-17 22:32:11 -0700

    adding audio input

--- src/ofApp.h
+++ src/ofApp.h
@@ -20,9 +20,23 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+        void audioIn(float * input, int bufferSize, int nChannels);
     
         // Pd
         AppCore core;
+    
+        // Audio
+        vector <float> left;
+        vector <float> right;
+        vector <float> volHistory;
+        
+        int 	bufferCounter;
+        int 	drawCounter;
+        
+        float smoothedVol;
+        float scaledVol;
+        
+        ofSoundStream soundStream;
     
         // Camera
         ofVideoGrabber  camera;

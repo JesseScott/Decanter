ed4405d98548052e92ac038fa15d5468dfe9be40
Jesse Scott <jesse@memelab.ca>
2013-10-23 21:27:28 -0700

    changed height

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1920, 800, OF_WINDOW);
+	ofSetupOpenGL(1920, 480, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
